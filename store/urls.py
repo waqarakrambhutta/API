8555ae8d63e436c50313c9fa93edd3098547801f
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-02-07 21:17:38 +0500

    API views

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('products/',views.product_list)
+    path('products/',views.product_list),
+    # path('product/<id>/',views.product_detail)
+    #this will also works if we put letter a or anything which is not in rules.
+    path('products/<int:<id>/',views.product_detail)
 ]
