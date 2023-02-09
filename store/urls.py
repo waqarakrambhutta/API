c0bb8492488b4e75cef161596f5835547e490c8c
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-02-09 21:07:36 +0500

    serializing objects

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,5 @@ urlpatterns=[
     path('products/',views.product_list),
     # path('product/<id>/',views.product_detail)
     #this will also works if we put letter a or anything which is not in rules.
-    path('products/<int:<id>/',views.product_detail)
+    path('products/<int:id>/',views.product_detail)
 ]
--- store/views.py
+++ store/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from rest_framework import status
+from .serializers import ProductSerializer
 # Create your views here.
 
 @api_view()
 def product_list(request):
-    return Response('ok')
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
+ 
 @api_view()
 def product_detail(request,id):
-    return Response(id)
- 
+    product=get_object_or_404(Product,pk=id)
+    serializers = ProductSerializer(product)
+    return Response(serializers.data)
+
+    
+    
+    
+# def product_detail(request,id):
+#     try:
+#             product=Product.objects.get(pk=id)
+#             serializers = ProductSerializer(product)
+#             return Response(serializers.data)
+#     except Product.DoesNotExist:
+#          return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+# def product_detail(request,id):
+#     product=Product.objects.get(pk=id)
+#     serializers = ProductSerializer(product)
+    
+#     return Response(serializers.data)
