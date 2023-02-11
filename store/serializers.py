d0968e701fd2561173577f433160d84f26337b7c
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-02-11 22:34:20 +0500

    model serializer.

--- store/serializers.py
+++ store/serializers.py
@@ -7,10 +7,15 @@ class CollectionSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
 
 
-class ProductSerializer(serializers.Serializer):
-    id= serializers.IntegerField()
-    title  = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2,source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title','unit_price','price_with_tax','collection']
+        
+    price_with_tax=serializers.SerializerMethodField(method_name='include_tax')
+    def include_tax(self,product):
+        return product.unit_price * Decimal(1.1)
+    
     collection = serializers.HyperlinkedRelatedField(
         queryset=Collection.objects.all(),
         view_name='collection-detail'
