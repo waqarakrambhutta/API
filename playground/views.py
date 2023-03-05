f699dc685c71280e6f8e193228959d36d673e3c1
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-03-05 20:42:51 +0500

    transactions

--- playground/views.py
+++ playground/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from store.models import Product,Collection
 from django.db.models import Q,F,Count,Max,Min,Value,Func,CharField,ExpressionWrapper,FloatField
 from django.db.models.functions import Concat
+from django.db import transaction
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(pk__range=(900,1001)).delete()
+    with transaction.atomic():
+        Product.objects.filter(pk__range=(852,855)).delete()
+        Collection.objects.filter(pk=16)
 
 
-    return render(request, 'hello.html',{'name':'Waqar','result':list(queryset)})
+
+
+    return render(request, 'hello.html',{'name':'Waqar'})
+                                        #  ,'result':list(queryset)})
