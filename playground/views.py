721f631a0f4b34c6c387213948c1f47750cc3a71
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-03-05 18:05:51 +0500

    Revision

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from store.models import Product
-from django.db.models import Q
+from store.models import Product,Collection
+from django.db.models import Q,F,Count,Max,Min,Value,Func,CharField,ExpressionWrapper,FloatField
+from django.db.models.functions import Concat
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(~Q(unit_price__gt=10)).first()
+    queryset = Product.objects.filter(pk__range=(900,1001)).delete()
 
 
-    return render(request, 'hello.html',{'name':'Waqar'})
+    return render(request, 'hello.html',{'name':'Waqar','result':list(queryset)})
