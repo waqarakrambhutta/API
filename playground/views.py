0f60f078d862ac6aa7d8cf09055b7d22c6cdee8b
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-03-05 14:15:40 +0500

    bitwise operators

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from store.models import Product
+from django.db.models import Q
 
 
 def say_hello(request):
-    product = Product.objects.filter(id=5).first()
+    queryset = Product.objects.filter(~Q(unit_price__gt=10)).first()
+
 
     return render(request, 'hello.html',{'name':'Waqar'})
