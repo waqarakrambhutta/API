02d4b8cef77128b94ac83d61824750e2e970a90d
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-03-06 20:57:59 +0500

    excecuting raw sql queries using cursor

--- playground/views.py
+++ playground/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from store.models import Product,Collection,Order,OrderItem
 from django.db.models import Q,F,Count,Max,Min,Value,Func,CharField,ExpressionWrapper,FloatField
 from django.db.models.functions import Concat
-from django.db import transaction
+from django.db import transaction,connection
 
 
 def say_hello(request):
-    queryset= Product.objects.raw('SELECT * FROM store_product\
-                                  order by inventory')
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO store_collection (id,title) VALUES (14,"tube well")')
+    cursor.close()
 
-
-    return render(request, 'hello.html',{'name':'Waqar','result':list(queryset)})
+    # return render(request, 'hello.html',{'name':'Waqar','result':list(queryset)})
+    return render(request, 'hello.html',{'name':'Waqar'})
