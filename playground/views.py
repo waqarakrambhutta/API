2f122bb5dc0bdf91de96359e878c7a177a5291f8
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-03-06 14:46:27 +0500

    transactions

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from store.models import Product,Collection
+from store.models import Product,Collection,Order,OrderItem
 from django.db.models import Q,F,Count,Max,Min,Value,Func,CharField,ExpressionWrapper,FloatField
 from django.db.models.functions import Concat
 from django.db import transaction
@@ -7,9 +7,16 @@ from django.db import transaction
 
 def say_hello(request):
     with transaction.atomic():
-        Product.objects.filter(pk__range=(852,855)).delete()
-        Collection.objects.filter(pk=16)
+        order = Order()
+        order.customer_id= 1
+        order.save()
 
+        item =  OrderItem()
+        item.order_id=1
+        item.unit_price=1
+        item.quantity= 4
+        item.product_id =1 
+        item.save()
 
 
 
