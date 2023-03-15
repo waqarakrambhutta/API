89c95ca4ba855d1626b2dac094de2fc5f4602325
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-03-15 19:31:57 +0500

    Logging in

--- storefront/settings.py
+++ storefront/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -161,4 +162,8 @@ DJOSER = {
             'user_create': 'core.serializers.UserCreateSerializer'
             #here we write the default serializers in the documentation.
     }
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1)
 }
