9d28160bf5c445751b63f09cdf97ccfd69d006ae
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-03-28 12:17:11 +0500

    for streek

--- store/views.py
+++ store/views.py
@@ -126,13 +126,6 @@ class OrderViewset(ModelViewSet):
 
 
 
-
-
-
-
-
-
-
 # from django.shortcuts import get_object_or_404
 # from django_filters.rest_framework import DjangoFilterBackend
 # from django.db.models import Count
